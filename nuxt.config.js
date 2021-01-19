06e29cb1ce894214f1f9e22bc8d9a08fb990b439
baklai <d.n.baklai@gmail.com>
2021-01-19 08:50:12 +0200

    fix: host

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,6 @@
 import colors from "vuetify/es5/util/colors";
 
-const HOST = "baklai.github.io";
+const HOST = `baklai.github.io/${process.env.npm_package_name}`;
 
 export default {
   ssr: false,
