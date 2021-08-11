836087dfb736b24b32fdfd346624bdf88b9bc64a
Dmitrii <d.n.baklai@gmail.com>
2021-08-11 10:19:03 +0300

    fix Though the "loose" option

--- nuxt.config.js
+++ nuxt.config.js
@@ -110,5 +110,13 @@ export default {
     },
   },
 
-  build: {},
+  build: {
+    babel: {
+      plugins: [
+        ["@babel/plugin-proposal-class-properties", { loose: true }],
+        ["@babel/plugin-proposal-private-methods", { loose: true }],
+        ["@babel/plugin-proposal-private-property-in-object", { loose: true }],
+      ],
+    },
+  },
 };
