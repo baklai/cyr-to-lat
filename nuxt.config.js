0606b2d548e2e0ae9a678d530f03e3f953be3269
Dmitrii <d.n.baklai@gmail.com>
2021-08-22 13:09:17 +0300

    fix cash

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,6 @@
-import colors from "vuetify/es5/util/colors";
+import colors from 'vuetify/es5/util/colors';
 
-const HOST = "https://baklai.github.io";
+const HOST = 'https://baklai.github.io';
 
 /// https://isometric.online/
 
@@ -9,118 +9,118 @@ export default {
 
   ssr: false,
 
-  target: "static",
+  target: 'static',
 
   cli: {
     badgeMessages: [
       `Application: ${process.env.npm_package_name.toUpperCase()} v${
         process.env.npm_package_version
-      }`,
-    ],
+      }`
+    ]
   },
 
   router: {
     base: `/${process.env.npm_package_name}/`,
-    prefetchLinks: false,
+    prefetchLinks: false
   },
 
   head: {
     titleTemplate: `%s • ${process.env.npm_package_author_name}`,
-    title: "Cyr To Lat",
+    title: 'Cyr To Lat',
     meta: [
-      { charset: "utf-8" },
+      { charset: 'utf-8' },
       {
-        hid: "description",
-        name: "description",
-        content: process.env.npm_package_description || "",
-      },
+        hid: 'description',
+        name: 'description',
+        content: process.env.npm_package_description || ''
+      }
     ],
     link: [
       {
-        rel: "icon",
-        type: "image/x-icon",
-        href: `/${process.env.npm_package_name}/favicon.ico`,
-      },
-    ],
+        rel: 'icon',
+        type: 'image/x-icon',
+        href: `/${process.env.npm_package_name}/favicon.ico`
+      }
+    ]
   },
 
-  loading: { color: "#2196f3" },
+  loading: { color: '#2196f3' },
 
-  css: ["@mdi/font/css/materialdesignicons.min.css", "~/assets/variables.css"],
+  css: ['@mdi/font/css/materialdesignicons.min.css', '~/assets/variables.css'],
 
   plugins: [
-    { src: "~/plugins/themeMode", mode: "client" },
-    { src: "~/plugins/clipboard.js", mode: "client" },
+    { src: '~/plugins/themeMode', mode: 'client' },
+    { src: '~/plugins/clipboard.js', mode: 'client' }
   ],
 
   components: true,
 
-  buildModules: ["@nuxtjs/vuetify"],
+  buildModules: ['@nuxtjs/vuetify'],
 
-  modules: ["@nuxtjs/pwa", "@nuxtjs/meta", "@nuxtjs/toast"],
+  modules: ['@nuxtjs/pwa', '@nuxtjs/meta', '@nuxtjs/toast'],
 
   pwa: {
     meta: {
-      charset: "utf-8",
-      viewport: "width=device-width, initial-scale=1",
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
       mobileApp: true,
       mobileAppIOS: true,
-      appleStatusBarStyle: "black",
+      appleStatusBarStyle: 'black',
       favicon: true,
-      name: "Converter Cyrillic To Latin",
+      name: 'Converter Cyrillic To Latin',
       author: process.env.npm_package_author_name,
       description: process.env.npm_package_description,
-      theme_color: "#fff",
-      lang: "ru",
-      ogType: "website",
+      theme_color: '#fff',
+      lang: 'ru',
+      ogType: 'website',
       ogHost: HOST,
       ogImage: {
         path: `/${process.env.npm_package_name}/icon.png`,
-        width: "50",
-        height: "50",
-        type: "image/png",
-      },
+        width: '50',
+        height: '50',
+        type: 'image/png'
+      }
     },
     manifest: {
-      name: "Converter Cyrillic To Latin",
-      short_name: "Cyr To Lat",
+      name: 'Converter Cyrillic To Latin',
+      short_name: 'Cyr To Lat',
       description: process.env.npm_package_description,
-      lang: "ru",
-      display: "standalone",
-      background_color: "#fff",
+      lang: 'ru',
+      display: 'standalone',
+      background_color: '#fff',
       start_url: `/${process.env.npm_package_name}/`,
-      useWebmanifestExtension: false,
-    },
+      useWebmanifestExtension: false
+    }
   },
 
   toast: {
-    type: "default",
-    theme: "toasted-primary",
-    position: "bottom-right",
-    icon: "alert-circle-outline",
+    type: 'default',
+    theme: 'toasted-primary',
+    position: 'bottom-right',
+    icon: 'alert-circle-outline',
     duration: 1000,
-    iconPack: "mdi",
+    iconPack: 'mdi',
     register: [
       {
-        name: "errorDefault",
-        message: "Oops... Something went wrong",
+        name: 'errorDefault',
+        message: 'Oops... Something went wrong',
         options: {
-          type: "error",
-          duration: 2000,
-        },
-      },
-    ],
+          type: 'error',
+          duration: 2000
+        }
+      }
+    ]
   },
 
   vuetify: {
-    customVariables: ["~/assets/variables.scss"],
+    customVariables: ['~/assets/variables.scss'],
     treeShake: true,
     icons: {
-      iconfont: "mdi",
+      iconfont: 'mdi'
     },
     theme: {
       options: {
-        customProperties: true,
+        customProperties: true
       },
       dark: false,
       themes: {
@@ -131,20 +131,25 @@ export default {
           info: colors.teal.lighten1,
           warning: colors.amber.base,
           error: colors.deepOrange.accent4,
-          success: colors.green.accent3,
+          success: colors.green.accent3
         },
-        light: {},
-      },
-    },
+        light: {}
+      }
+    }
+  },
+
+  workbox: {
+    cacheAssets: false,
+    offline: false
   },
 
   build: {
     babel: {
       plugins: [
-        ["@babel/plugin-proposal-class-properties", { loose: true }],
-        ["@babel/plugin-proposal-private-methods", { loose: true }],
-        ["@babel/plugin-proposal-private-property-in-object", { loose: true }],
-      ],
-    },
-  },
+        ['@babel/plugin-proposal-class-properties', { loose: true }],
+        ['@babel/plugin-proposal-private-methods', { loose: true }],
+        ['@babel/plugin-proposal-private-property-in-object', { loose: true }]
+      ]
+    }
+  }
 };
